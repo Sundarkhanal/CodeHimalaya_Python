fd5464162c486c401fc2da4e46fe05afb0e5fbc6
Sundar Khanal <sundarkhanal62@gmail.com>
2024-07-19 12:51:13 +0545

    ada

---
+++ functiontask.py
@@ -0,0 +1,12 @@
+# def greeting(name):
+#     return f"Hello, {name} nice to meet you"
+
+# user_name = input("Enter your name: ")
+# print(greeting(user_name))
+
+
+def greeting(name):
+    return f"Hello, {name} nice to meet you"
+
+print(greeting("Sundar"))
+print(greeting("Kushal"))
