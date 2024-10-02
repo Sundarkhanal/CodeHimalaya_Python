d35790d80dfe8f082cb6c84256e052064313dbaf
Dikshyant Baral <baraldikshyant@gmail.com>
2024-10-02 11:33:21 +0545

    first commit

--- variable.py
+++ variable.py
@@ -1,5 +1,6 @@
 a = 15
 b = 4
+c=9
 print(a+b)
 print(a-b)
 print(a*b)
@@ -7,6 +8,7 @@ print(a/b)
 print(a//b)    #remove float value
 print(a**b)    # power
 print(a%b)     #remainder
+print(c)
 
 
 
